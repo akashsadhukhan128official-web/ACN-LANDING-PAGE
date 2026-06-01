8899e81e13ff7616e67c46badb5ff250387a2cee
ACN Admin <admin@acnbroadband.com>
2026-06-02 00:21:39 +0530

    Clear old Firebase setup to prepare for new configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD63IDahWPEXwCHV5VZTHensiJc0GKJvK4",
-    authDomain: "landing-final-acn.firebaseapp.com",
-    databaseURL: "https://landing-final-acn-default-rtdb.firebaseio.com",
-    projectId: "landing-final-acn",
-    storageBucket: "landing-final-acn.firebasestorage.app",
-    messagingSenderId: "951269356945",
-    appId: "1:951269356945:web:0cb9c11201ef0053635630",
-    measurementId: "G-MQWFD8RC4C"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Check if Firebase is available globally (from CDN)
