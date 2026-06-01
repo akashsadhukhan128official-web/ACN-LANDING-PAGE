966ff0895ca1f64b3c4d88138983ef350b098c3a
ACN Admin <admin@acnbroadband.com>
2026-06-02 00:27:20 +0530

    Update Firebase configuration credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDMKzg3d9-e7twvGHq2xjMUOwmM9uEqQqY",
+    authDomain: "acn-landing-login.firebaseapp.com",
+    databaseURL: "https://acn-landing-login-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "acn-landing-login",
+    storageBucket: "acn-landing-login.firebasestorage.app",
+    messagingSenderId: "398411390456",
+    appId: "1:398411390456:web:522a91458be1e554a30a0d",
+    measurementId: "G-K655SL1GV7"
 };
 
 // Check if Firebase is available globally (from CDN)
